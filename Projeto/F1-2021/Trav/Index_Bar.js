2db3badd4bf9488a1c7935a843f68cb62b0fe039
Rafa548 <rafael3vilaca30@gmail.com>
2022-11-11 20:57:05 +0000

    first commit

---
+++ Projeto/F1-2021/Trav/Index_Bar.js
@@ -0,0 +1,20 @@
+﻿// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    var InputButt = document.querySelector("#SearchVal");
+    var SearchButton = document.querySelector("#searchButton")
+    self.searchall = ko.observable('');
+    //--- Page Events
+    InputButt.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            SearchButton.click();
+        }
+    });
+};
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});
---
+++ Projeto/F1-2021/Trav/seasonStats.js
@@ -0,0 +1,101 @@
+﻿// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    var InputButt = document.querySelector("#SearchVal");
+    var SearchButton = document.querySelector("#searchButton")
+    self.baseUri = ko.observable('http://192.168.160.58/Formula1/api/Statistics/Season?year=');
+    self.displayName = 'Season Stats';
+    self.error = ko.observable('');
+    self.passingMessage = ko.observable('');
+    //--- Data Record
+    self.Year = ko.observable('');
+    self.Races = ko.observable('');
+    self.Countries = ko.observable('')
+    self.Constructors = ko.observable('')
+    self.Drivers = ko.observable('')
+    self.DriverStandings = ko.observableArray('')
+    self.ConstructorStandings = ko.observableArray('')
+    self.searchall = ko.observable('')
+
+    //--- Page Events
+    self.activate = function (id) {
+        console.log('CALL: getSeasonsdet...');
+        var composedUri = self.baseUri() + id;
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            console.log(data);
+            self.Year(data.Year);
+            self.Races(data.Races);
+            self.Countries(data.Countries);
+            self.Constructors(data.Constructors);
+            self.Drivers(data.Drivers);
+            self.DriverStandings(data.DriverStandings);
+            self.ConstructorStandings(data.ConstructorStandings);
+            hideLoading();
+        });
+    };
+    InputButt.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            SearchButton.click();
+        }
+    });
+    //--- Internal functions
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX Call[" + uri + "] Fail...");
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+
+    }
+    function showLoading() {
+        $('#myModal').modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+    //--- start ....
+    showLoading();
+    var pg = getUrlParameter('year');
+    console.log(pg);
+    if (pg == undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+};
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});
